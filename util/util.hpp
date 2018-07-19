c8d981933e151ebd240c30d8c3d2e3e0953c5d94
lyfysyn <lyfysyn@163.com>
2018-07-19 17:58:13 +0800

    implement to to_string(double, int).

---
+++ util/util.hpp
@@ -0,0 +1,19 @@
+#include <sstream>
+#include <sstream>
+
+namespace util
+{
+
+inline std::string to_string(double v, int precision = 3)
+{
+	std::stringstream ss;
+	ss.precision(precise);
+	ss << std::fixed << v;
+	std::string out;
+	ss >> out;
+
+	return out;
+}
+
+}
+
